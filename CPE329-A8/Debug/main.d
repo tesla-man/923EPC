b045b372fbf4da5372e5d2eaa01bbe0761e0d4cf
tesla-man <jlcontovasilis@gmail.com>
2018-05-07 19:48:37 -0700

    completed A8

--- CPE329-A8/Debug/main.d
+++ CPE329-A8/Debug/main.d
@@ -1,6 +1,7 @@
 # FIXED
 
 main.obj: ../main.c
+main.obj: C:/Users/Contovasilis/Desktop/CPE329/utils/spi.h
 main.obj: C:/ti/ccsv7/ccs_base/arm/include/msp.h
 main.obj: C:/ti/ccsv7/ccs_base/arm/include/msp432p401r.h
 main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.7.LTS/include/stdint.h
@@ -10,10 +11,11 @@ main.obj: C:/ti/ccsv7/ccs_base/arm/include/msp432p401r_classic.h
 main.obj: C:/ti/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h
 main.obj: C:/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_compiler.h
 main.obj: C:/ti/ccsv7/ccs_base/arm/include/system_msp432p401r.h
+main.obj: C:/Users/Contovasilis/Desktop/CPE329/utils/uart.h
 main.obj: C:/Users/Contovasilis/Desktop/CPE329/utils/delay.h
-main.obj: C:/Users/Contovasilis/Desktop/CPE329/utils/spi.h
 
 ../main.c: 
+C:/Users/Contovasilis/Desktop/CPE329/utils/spi.h: 
 C:/ti/ccsv7/ccs_base/arm/include/msp.h: 
 C:/ti/ccsv7/ccs_base/arm/include/msp432p401r.h: 
 C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.7.LTS/include/stdint.h: 
@@ -23,5 +25,5 @@ C:/ti/ccsv7/ccs_base/arm/include/msp432p401r_classic.h:
 C:/ti/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h: 
 C:/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
 C:/ti/ccsv7/ccs_base/arm/include/system_msp432p401r.h: 
+C:/Users/Contovasilis/Desktop/CPE329/utils/uart.h: 
 C:/Users/Contovasilis/Desktop/CPE329/utils/delay.h: 
-C:/Users/Contovasilis/Desktop/CPE329/utils/spi.h: 
--- utils/delay.h
+++ utils/delay.h
@@ -17,10 +17,6 @@
 #define FREQ_24_MHZ     (uint16_t)240
 #define FREQ_48_MHZ     (uint16_t)480
 
-#ifndef FCPU
-#define FCPU    FREQ_12_MHZ
-#endif
-
 inline delay_ms(uint32_t time);
 inline delay_us(uint32_t time);
 void set_DCO(uint16_t freq);
